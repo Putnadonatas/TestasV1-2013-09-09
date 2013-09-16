d3199326af882d631d3251f868c565237b0a5c5e
putnadonatas <putnadonatas@gmail.com>
2013-09-16 08:43:41 +0300

    papildytas 2013 9 16

---
+++ TestasV2.m
@@ -0,0 +1,14 @@
+function [ f,f_isv,f_isv2 ] = TestasV2(x)
+%TestasV1 Uzdavynio aprasas
+% input(ivedimo parametrai) :   x - realus vienmatis vektorius
+%Output(isvesties parametrai):
+%Funkcija x,f(x) pirmoji reiksme.
+%Funkcija x,f'(x) antroji reiksme.
+%Funkcija x,f''(x) trecioji reiksme.
+%pvz: function [ f,f_isv,f_isv2 ] = TestasV1([1,1]) arba 
+%x=[1,1]; [ f,f_isv,f_isv2 ] = TestasV2(x)
+f= sin(x)+cos(x);
+f_isv=cos(x)-sin(x);
+f_isv2=-sin(x)-cos(x);
+end
+
